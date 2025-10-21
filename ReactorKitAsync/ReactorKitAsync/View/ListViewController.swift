6168008e8892ee5f4dc032f0bc9f606ae634e619
talq44 <talq44@gmail.com>
2025-10-21 23:26:58 +0900

    동작 연결

--- ReactorKitAsync/ReactorKitAsync/View/ListViewController.swift
+++ ReactorKitAsync/ReactorKitAsync/View/ListViewController.swift
@@ -10,6 +10,14 @@ final class ListViewController: UIViewController {
     var disposeBag = DisposeBag()
     
     private let collectionView = UICollectionView()
+    private let refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        return refreshControl
+    }()
+    private let indicatorView = UIActivityIndicatorView(style: .large)
+    private let emptyImage = UIImageView(
+        image: UIImage(systemName: "exclamationmark.magnifyingglass")
+    )
     
     init(reactor: ListViewReactor) {
         super.init(nibName: nil, bundle: nil)
@@ -23,10 +31,22 @@ final class ListViewController: UIViewController {
     
     override func loadView() {
         super.loadView()
+        
         view.addSubview(collectionView)
+        view.addSubview(indicatorView)
+        view.addSubview(emptyImage)
+        
         collectionView.snp.makeConstraints { make in
             make.directionalEdges.equalTo(view.safeAreaLayoutGuide)
         }
+        indicatorView.snp.makeConstraints { make in
+            make.center.equalTo(view.safeAreaLayoutGuide)
+        }
+        emptyImage.snp.makeConstraints { make in
+            make.center.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        collectionView.refreshControl = refreshControl
     }
     
     override func viewDidLoad() {
@@ -43,6 +63,19 @@ extension ListViewController: ReactorKit.View {
     }
     
     private func bindAction(reactor: ListViewReactor) {
+        rx.viewDidLoad
+            .map { Reactor.Action.refresh }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        refreshControl.rx.controlEvent(.valueChanged)
+            .do(onNext: { [weak self] _ in
+                self?.refreshControl.beginRefreshing()
+            })
+            .map { Reactor.Action.refresh}
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
         collectionView.rx.modelSelected(ListViewState.Item.self)
             .map { Reactor.Action.selectItem(id: $0.id) }
             .bind(to: reactor.action)
@@ -53,11 +86,26 @@ extension ListViewController: ReactorKit.View {
         reactor.state.map { $0.items }
             .distinctUntilChanged()
             .observe(on: MainScheduler.instance)
+            .do(onNext: { [weak self] _ in
+                self?.refreshControl.endRefreshing()
+            })
             .bind(to: collectionView.rx.items(
                 cellIdentifier: String(describing: Cell.self),
                 cellType: Cell.self
             )) { _, _, _ in
                 
             }.disposed(by: disposeBag)
+        
+        reactor.state.map { $0.isShowLoading }
+            .distinctUntilChanged()
+            .map { !$0 }
+            .bind(to: indicatorView.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.isShowEmpty }
+            .distinctUntilChanged()
+            .map { !$0 }
+            .bind(to: emptyImage.rx.isHidden)
+            .disposed(by: disposeBag)
     }
 }
